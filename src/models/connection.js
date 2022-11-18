43df8da317a0ede4f8c4aa39968dcf26e8e6a5eb
Windson Macedo <windsondev@hotmail.com>
2022-11-18 16:23:00 -0300

    .env config

--- src/models/connection.js
+++ src/models/connection.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 const connection = mysql.createPool({
-  host: 'db',
-  port: 3306,
-  user: 'root',
-  password: 'password',
-  database: 'StoreManager',
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE || 'StoreManager',
+  port: process.env.MYSQL_PORT,
 });
 
 module.exports = connection;
