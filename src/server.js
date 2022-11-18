3f313331aac5464af22b67b5b5a23961ad23c7b3
Windson Macedo <windsondev@hotmail.com>
2022-11-18 18:41:39 -0300

    test 3

--- src/server.js
+++ src/server.js
@@ -1,19 +1,8 @@
 const app = require('./app');
-const connection = require('./models/connection');
 require('dotenv').config();
 
 // não altere esse arquivo, essa estrutura é necessária para à avaliação do projeto
 
 app.listen(process.env.PORT, () => {
   console.log(`Escutando na porta ${process.env.PORT}`);
-});
-
-const port = 3306;
-
-app.listen(port, async () => {
-  // O código abaixo é para testarmos a comunicação com o MySQL
-  const [result] = await connection.execute('SELECT 1');
-  if (result) {
-    console.log('MySQL connection OK');
-  }
 });
